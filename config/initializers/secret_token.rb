a70a377c14b905177c4f6e04761565a1e9fcd104
hima cherukuru <hima.cherukuru@gmail.com>
2012-07-30 17:08:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarkeMadchen::Application.config.secret_token = '4ed8507d932f6a822e104fb12f671729d71250d6dd2e63680b0bfa4fc31e3f5cc542d05cd146b4c1be32a77069313435d16c18f0901e2851d7cf72733baa0bf9'
